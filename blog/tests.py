6ebead19a564afe5fa085cf717f75c32378dc4f5
Dakori9480 <kgh9480@naver.com>
2024-03-08 12:13:09 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/tests.py
+++ blog/tests.py
@@ -9,14 +9,14 @@ class TestView(TestCase):
     def test_post_list(self):
         
         # 1.1 포스트 목록 페이지를 가져온다
-        response = self.client.get("/blog/")
+        response = self.client.get('/blog/')
         
         # 1.2 정상적으로 페이지가 로드된다
         self.assertEqual(response.status_code, 200)
         
         # 1.3 페이지 타이틀은 'Blog'이다
-        soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Blog')
+        soup = BeautifulSoup(response.content,'html.parser')
+        self.assertEqual(soup.title.text,' Blog ')
         
         # 1.4 내비게이션 바가 있다
         navbar = soup.nav
@@ -29,7 +29,7 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 0)
         
         # 2.2 '아직 게시물이 없습니다' 라는 문구가 보인다
-        main_area = soup.find('div', id='main_area.text')
+        main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
         
         # 3.1 게시물이 2개가 있다면
@@ -39,7 +39,7 @@ class TestView(TestCase):
         )
         post_002 = Post.objects.create(
             title = '두 번째 포스트입니다.',
-            content = '1등이 전부는 아니잖아요?'
+            content = '1등이 전부는 아니잖아요?',
         )
         self.assertEqual(Post.objects.count(), 2)
         
@@ -49,9 +49,9 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         
         # 3.3 메인 영역에 포스트 2개의 타이틀이 존재한다
-        main_area = soup.find(d='div' , id='main-area')
-        self.assertIn(post_001, main_area.text)
-        self.assertIn(post_002, main_area.text)
+        main_area = soup.find('div' , id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
         
         # 3.4 '아직 게시물이 없습니다' 라는 문구는 더 이상 보이지 않는다
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
@@ -62,7 +62,7 @@ class TestView(TestCase):
         # 1.1 포스트가 하나 있다.
         post_001 = Post.objects.create(
             title = "첫 번째 포스트입니다",
-            content = 'Hello World. We are the world',
+            content = 'Hello World. We are the world',  
         )
         
         # 1.2 그 포스트의 url은 '/blog/1/' 이다
@@ -76,7 +76,7 @@ class TestView(TestCase):
         
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다
         navbar = soup.nav
-        self.assertIn('blog', navbar.text)
+        self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
         
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다
