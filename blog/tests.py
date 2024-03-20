8b34dbb8405bd98d35421c317898c85df11a9c99
Dakori9480 <kgh9480@naver.com>
2024-03-21 00:25:38 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/tests.py
+++ blog/tests.py
@@ -189,11 +189,35 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn(self.tag_hello.name, main_area.text)
         
-        self.assertIn(self.post_001.title, main_area.text)
+        self.assertIn(self.post_001.title, main_area    .text)
         self.assertNotIn
         
+    def test_create_post(self):
+        # 로그인 하지않으면 status_code가 200이 되면 안된다.
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
         
+        # 로그인을 한다.
+        self.client.login(username='dakori', password='somepassword')
         
+        response = self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        self.assertEqual(' Create Post - Blog ', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Create New Post', main_area.text)
+        
+        self.client.post(
+            '/blog/create_post/',
+            {
+                'title' : 'Post Form 만들기',
+                'content' : 'Post Form 페이지를 만듭니다.',
+            }
+        )
+        last_post = Post.objects.last()
+        self.assertEqual(last_post.title, "Post Form 만들기")
+        self.assertEqual(last_post.author.username, 'dakori')
         
         
         
